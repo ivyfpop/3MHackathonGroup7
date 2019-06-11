f6ab6ce0481aaa7dbe52c0699ac1a713a103d0a7
Michael Fink <ivyfpop.x@gmail.com>
2019-06-11 12:42:53 -0500

    j

--- add_numbers.js
+++ add_numbers.js
@@ -30,8 +30,9 @@ function add() {
 	}
 	var mprResult = 1;
 
-    document.getElementById('result').innerHTML =.39*0.818/(1-(length*2.125+(width-2.125)*2.125)/(length*width))*mprResult;
-    return false;
+    //document.getElementById('result').innerHTML =.39*0.818/(1-(length*2.125+(width-2.125)*2.125)/(length*width))*mprResult;
+	document.getElementById('result').innerHTML =.39*0.818/(1-(length*2.125+(width-2.125)*2.125)/(length*width));
+   return false;
 }
 
 document.getElementById('go').addEventListener('click', add);
