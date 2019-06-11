ac34a0d5123584b5806532e2f51201f64fe8787d
Michael Fink <ivyfpop.x@gmail.com>
2019-06-11 12:38:16 -0500

    Another

--- add_numbers.js
+++ add_numbers.js
@@ -1,10 +1,36 @@
 "use strict";
 
 function add() {
-    var x = Number(document.getElementById('x').value);
-    var y = Number(document.getElementById('y').value);
+	var length = Number(document.getElementById('length').value);
+    var width = Number(document.getElementById('width').value);
+	
+	var mpr = document.getElementById('mpr').value;
+	var mprResult;
+	
+	// Determine the correct mpr Result number
+	switch(mpr) {
+	  case "2800":
+	  	var mprResult = .25;
+		break;
+	  case "2400/2500":
+	  	var mprResult = .23;
+		break;
+	  case "2200":
+	  	var mprResult = .23;
+		break;
+	  case "1900":
+	  	var mprResult = .23;
+		break;
+	  case "1500":
+	  	var mprResult = .23;
+		break;
+	  case "1200 Carbon":
+	  	var mprResult = .23;
+		break;
+	}
+	
 
-    document.getElementById('result').innerHTML = x+y;
+    document.getElementById('result').innerHTML =.39*0.818/(1-(length*2.125+(width-2.125)*2.125)/(length*width))*mprResult;
     return false;
 }
 
