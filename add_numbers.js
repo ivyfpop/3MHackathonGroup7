4b172f33f43defcba4bdee4641d1408d58871885
Michael Fink <ivyfpop.x@gmail.com>
2019-06-11 14:28:12 -0500

    Update add_numbers.js

--- add_numbers.js
+++ add_numbers.js
@@ -6,49 +6,10 @@ function add() {
 	var mprResult = Number(document.getElementById('mpr').value);
 	var cfmResult = Number(document.getElementById('cfm').value);
 
-	var result = cfmResult*0.818/(1-2.125(length+width-2.125)/(length*width))*mprResult;
+
+	var result = cfmResult*0.818/(1-(length*2.125+(width-2.125)*2.125)/(length*width))*mprResult;
     document.getElementById('result').innerHTML = result.toFixed(2);
     return false;
 }
 
 document.getElementById('go').addEventListener('click', add);
-/*
-"use strict";
-
-function add() {
-	var length = Number(document.getElementById('length').value);
-    var width = Number(document.getElementById('width').value);
-	
-	
-	var mpr = document.getElementById('mpr').value;
-	var mprResult;
-	// Determine the correct mpr Result number
-	switch(mpr) {
-	  case "2800":
-	  	var mprResult = .25;
-		break;
-	  case "2400/2500":
-	  	var mprResult = .23;
-		break;
-	  case "2200":
-	  	var mprResult = .23;
-		break;
-	  case "1900":
-	  	var mprResult = .23;
-		break;
-	  case "1500":
-	  	var mprResult = .23;
-		break;
-	  case "1200 Carbon":
-	  	var mprResult = .23;
-		break;
-	}
-	var mprResult = 1;
-
-    //document.getElementById('result').innerHTML =.39*0.818/(1-(length*2.125+(width-2.125)*2.125)/(length*width))*mprResult;
-    document.getElementById('result').innerHTML = length+width;
-   return false;
-}
-
-document.getElementById('go').addEventListener('click', add);
-*/
